cd338e5a4c1f34b5b68f35e2c5f0b93a271adf6e
G-Kavinesh <gkavinesh186@gmail.com>
2025-08-06 22:51:10 +0530

    Commit

--- server/api/index.js
+++ server/api/index.js
@@ -1,14 +1,59 @@
-// api/index.js
+// Import URI for PostgreSQL from config.js, defined in .env, safer way to encrypt keys
+const config = require("./config.js");
+const pool = require("./db/dbconn.js");
 
-const express = require('express');
+const path = require("path"); // to serve static files in production
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello from Express on Vercel!');
-});
+const blogRoutes = require("./routes/blogRouter.js"); // where the blogs requests handlers are listed
+const userRoutes = require("./routes/userRouter.js"); // where the users requests handlers are listed
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
-});
+// connect to PostgreSQL
+pool.connect()
+    .then((client) => {
+        client.release(); // release the connection back to the pool
+        console.log("Connected to PostgreSQL successfully");
+    }) // verify connection is established
+    .catch((err) => {
+        console.error("Failed to connect to PostgreSQL");
+        process.exit(1);
+    });
+
+app.use(express.json()); // To parse JSON bodies so i can access the properties object with body.name for example
+app.use(express.urlencoded({ extended: true })); // to parse URL-encoded bodies, like form submissions
+app.use(cors()); // This line allows all origins by default, usefull for Recat which might use a different port
+app.use(morgan("dev")); // logging middleware, prints out the request method and URL in the console in real time
+
+app.use("/api/blogs", blogRoutes); // This sets the base path for all blog routes
+app.use("/api/users", userRoutes); // This sets the base path for all user routes
+
+// Enable CORS for all routes
+
+// Optional: restrict to specific origin
+//  app.use(cors({ origin: 'http://localhost:3000'
+//      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//      credentials: true
+//  }));
+
+// Serve static files from the Vite dist folder
+if (process.env.NODE_ENV === "production") {
+    const clientBuildPath = path.join(__dirname, "../../client/dist");
+    app.use(express.static(clientBuildPath));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(clientBuildPath, "index.html"));
+    });
+}
+
+// Only start the server if not on Vercel (serverless)
+if (process.env.NODE_ENV !== "production" || !process.env.VERCEL) {
+    app.listen(config.port, () => {
+        console.log(`Server running on port ${config.port}`); // Express runs in a different port than PSQL
+    });
+}
 
 module.exports = app;
